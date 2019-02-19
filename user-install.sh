d276e84e8c360f1579e58264e02c06fd85b9fc08
WillForan <willforan@gmail.com>
2019-02-19 09:39:45 -0500

    update to look for .Xdefaults too

--- user-install.sh
+++ user-install.sh
@@ -8,14 +8,19 @@ ext_dir=$HOME/.urxvt/ext
 [ ! -r $ext_dir/resize-font  ] && ln -s $this_dir/resize-font $ext_dir/
 
 ## set .Xresources to use it
-Xrc=$HOME/.Xresources
-g=$(grep -n urxvt.perl-ext-common $Xrc 2>/dev/null)
+
+# .Xdefaults or .Xresources? if neither use .Xresources
+Xrc=($(find $HOME -maxdepth 1 -iname .Xdefaults -or -iname .Xresources))
+[ -z "$Xrc" ] && Xrc=$HOME/.Xresources
+
+# do we already have a line for urxvt
+g=$(grep -ni urxvt.perl-ext-common ${Xrc[@]} 2>/dev/null)
 
 # have no xres file or have no perl-ext line
-[ ! -r $Xrc -o -z "$g" ] && echo "consider:
+[ -z "$g" ] && echo "consider:
 echo 'urxvt.perl-ext-common: default,tabbed,matcher,resize-font,-tabbed' >> $Xrc" && exit
 
 # maybe have perl-ext but not resize-font
-echo $g | grep resize-font >/dev/null || echo "consider adding resize-font to $Xrc:$g"
+echo $g | grep resize-font >/dev/null || echo "consider adding 'resize-font' to $g (${Xrc[@]})"
 
 
